0b465a04444079924d8e4f929eaa3811c655f536
Vinicius dos Santos <viniciusidai@gmail.com>
2024-04-16 21:50:58 -0300

    Atualizando as strings e cores

--- app/src/main/java/com/sir_vinicius/appcardapio/MainActivity.kt
+++ app/src/main/java/com/sir_vinicius/appcardapio/MainActivity.kt
@@ -1,62 +1,63 @@
-package com.sir_vinicius.appcardapio
+    package com.sir_vinicius.appcardapio
 
-import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.sir_vinicius.appcardapio.databinding.ActivityMainBinding
+    import android.os.Bundle
+    import android.view.View
+    import android.view.ViewGroup
+    import android.widget.CheckBox
+    import android.widget.Toast
+    import androidx.appcompat.app.AppCompatActivity
+    import com.sir_vinicius.appcardapio.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding: ActivityMainBinding
+    class MainActivity : AppCompatActivity(), View.OnClickListener {
+        private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
-        binding.buttonOrder.setOnClickListener(this)
-    }
-
-    override fun onClick(view: View) {
-        when (view.id) {
-            R.id.button_Order -> realizarPedido()
+            binding.buttonOrder.setOnClickListener(this)
         }
-    }
 
-    private fun realizarPedido() {
-        var hasSelectedItems = false
-        val itemsSelecionados = StringBuilder()
-
-        val checkboxesContainer = binding.root.findViewById<ViewGroup>(R.id.checkboxes_container)
-        val checkboxes = checkboxesContainer.findCheckboxes()
-
-        for (checkbox in checkboxes) {
-            if (checkbox.isChecked) {
-                hasSelectedItems = true
-                itemsSelecionados.append("${checkbox.text}, ")
+        override fun onClick(view: View) {
+            when (view.id) {
+                R.id.button_Order -> realizarPedido()
             }
         }
 
-        if (!hasSelectedItems) {
-            Toast.makeText(this, "Selecione pelo menos um item antes de realizar o pedido", Toast.LENGTH_SHORT).show()
-            return
+        private fun realizarPedido() {
+            var hasSelectedItems = false
+            val itemsSelecionados = StringBuilder()
+
+            val checkboxesContainer = binding.root.findViewById<ViewGroup>(R.id.checkboxes_container)
+            val checkboxes = checkboxesContainer.findCheckboxes()
+
+            for (checkbox in checkboxes) {
+                if (checkbox.isChecked) {
+                    hasSelectedItems = true
+                    itemsSelecionados.append("${checkbox.text}, ")
+                }
+            }
+
+            if (!hasSelectedItems) {
+                Toast.makeText(this, "Selecione pelo menos um item antes de realizar o pedido", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            //Exibir itens selecionados
+            Toast.makeText(this, "Pedido realizado com sucesso! Itens selecionados: ${itemsSelecionados.trimEnd(',', ' ')}",  Toast.LENGTH_LONG).show()
         }
 
-        // Exibir itens selecionados
-        Toast.makeText(this, "Pedido realizado com sucesso! Itens selecionados: ${itemsSelecionados.trimEnd(',', ' ')}", Toast.LENGTH_LONG).show()
-    }
-    private fun ViewGroup.findCheckboxes(): List<CheckBox> {
-        val checkboxes = mutableListOf<CheckBox>()
-        for (i in 0 until childCount) {
-            val child = getChildAt(i)
-            if (child is ViewGroup) {
-                checkboxes.addAll(child.findCheckboxes())
-            } else if (child is CheckBox) {
-                checkboxes.add(child)
+        private fun ViewGroup.findCheckboxes(): List<CheckBox> {
+            val checkboxes = mutableListOf<CheckBox>()
+            for (i in 0 until childCount) {
+                val child = getChildAt(i)
+                if (child is ViewGroup) {
+                    checkboxes.addAll(child.findCheckboxes())
+                } else if (child is CheckBox) {
+                    checkboxes.add(child)
+                }
             }
+            return checkboxes
         }
-        return checkboxes
     }
-}
